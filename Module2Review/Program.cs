f53509e77a582e333098f2f43f4d4177916d4636
Alexis Boegner <alexisboegner@gmail.com>
2025-10-16 15:35:59 -0400

    Called the AddGrade() Method

--- Module2Review/Program.cs
+++ Module2Review/Program.cs
@@ -12,6 +12,9 @@ class Program
             ID = 001
         };
         students.Add(student1);
+        student1.AddGrade(93.7d);
+        student1.AddGrade(97.5d, 96.3d, 91.9d);
+        student1.AddGrade(99.7d);
         
         var student2 = new Student()
         {
@@ -19,6 +22,9 @@ class Program
             ID = 002
         };
         students.Add(student2);
+        student2.AddGrade(85.9d);
+        student2.AddGrade(83.6d,91.3d,87.9d);
+        student2.AddGrade(87.1d);
 
         var student3 = new Student()
         {
@@ -26,6 +32,9 @@ class Program
             ID = 003
         };
         students.Add(student3);
+        student3.AddGrade(87.6d);
+        student3.AddGrade(83.1d, 85.9d, 90.1d);
+        student3.AddGrade(85.0d);
 
         var student4 = new Student()
         {
@@ -33,5 +42,8 @@ class Program
             ID = 004
         };
         students.Add(student4);
+        student4.AddGrade(70.9d);
+        student4.AddGrade(77.3d, 73.5d, 79.0d);
+        student4.AddGrade(67.0d);
     }
 }
