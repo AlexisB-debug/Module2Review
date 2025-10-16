863eb4bd0d0ff64ce8832238eace4a546222f3f0
Alexis Boegner <alexisboegner@gmail.com>
2025-10-16 14:49:37 -0400

    Created List students, Created 4 instances of type Student, & Populated List students

--- Module2Review/Program.cs
+++ Module2Review/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        List<Student> students = new List<Student>();
+
+        var student1 = new Student()
+        {
+            Name = "Granger, Hermione Jean",
+            ID = 001
+        };
+        students.Add(student1);
+        
+        var student2 = new Student()
+        {
+            Name = "Malfoy, Draco Lucius",
+            ID = 002
+        };
+        students.Add(student2);
+
+        var student3 = new Student()
+        {
+            Name = "Potter, Harry James",
+            ID = 003
+        };
+        students.Add(student3);
+
+        var student4 = new Student()
+        {
+            Name = "Weasley, Ronald Bilius",
+            ID = 004
+        };
+        students.Add(student4);
     }
 }
